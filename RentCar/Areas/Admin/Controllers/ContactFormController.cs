df4d214def514b3a8c493040eb85a86388fcd9fc
vasif-student <vasifah@code.edu.az>
2022-04-24 23:56:08 +0400

    contact-form

---
+++ RentCar/Areas/Admin/Controllers/ContactFormController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentCar.DAL;
+using RentCar.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentCar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = RoleConstants.SuperAdmin)]
+    public class ContactFormController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ContactFormController(AppDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var contactForms = await _dbContext.ContactForms.ToListAsync();
+            return View(contactForms);
+        }
+    }
+}
